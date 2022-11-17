b783b11a0d7c166b59324000781b69f24bb4c876
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-11-17 17:48:03 -0600

    inquirer prompts

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+-- Import and require express and mysql2
+const express = require('express' )
+const mysql = require('mysql2');
+
+-- Write middlware
+
+app.use(express.urlencoded({ extended: false }));
+-- Using JSON data
+app.use(express.json());
+
---
+++ index.js
@@ -0,0 +1,10 @@
+//Require insomnia package
+const inquirer = require('inquirer');
+
+inquirer
+.prompt([{
+    type: 'input',
+    message: '',
+    
+}
+])
