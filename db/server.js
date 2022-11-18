b2f7a065bf3a44c77879b745aacfdd227e41c8c3
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-11-17 18:46:54 -0600

    update inquirer package

--- db/server.js
+++ db/server.js
@@ -1,8 +1,12 @@
-// Import and require express and mysql2
 const express = require("express");
-const mysql = require("mysql2");
-// Write middlware
+const app = express();
+const port = 3000;
 
-app.use(express.urlencoded({ extended: false }));
-// Using JSON data
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+// if err throw eerr
--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const inquirer = require('inquirer');
 
 inquirer
 .prompt([{
-    type: 'input',
-    message: '',
+    type:'list',
+    name:'view_choices',
+    message: 'What would you like to do?',
+    choices: ['View all departments', 'View all employees','Add a role','Update employee role']
     
 }
 ])
+
+.then((response) =>
+response.confirm === response.view_choices
+  ? console.log('Success!')
+  : console.log('You forgot your password already?!')
+);
