78292f7c06e62f75598f304dc5c2fcbaf758867b
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-11-19 11:20:52 -0600

    added dept and salary params

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO DEPT_INFO (id, dept_name)
 VALUES 
-       (001, "Audio Engineering"),
+       (001, "Engineering"),
        (002, "Finance"),
        (003, "Legal Services"),
        (004, "Sales"),
@@ -8,12 +8,12 @@ VALUES
        (006, "Human Resources")
      
        
-INSERT INTO ROLE_INFO (id, dept_name)
+INSERT INTO ROLE_INFO (role_id, dept_name, salary)
 VALUES 
-       (001, "Audio Engineer"),
-       (002, "Accounts Administrator"),
-       (003, "Paralegal"),
+       (001, "Audio Engineer", "Engineering", $60,000),
+       (002, "Accounts Administrator", "Finance",  $52,275),
+       (003, "Paralegal", "Legal Services", $78,478 ,
        (004, "Digital Assistant"),
-       (005, "Court Jester"),
-       (006, "Benefits Manager")
+       (005, "Court Jester", "Shenanigans", $90,000),
+       (006, "Benefits Manager", "Human Resources", $130,000)
      
