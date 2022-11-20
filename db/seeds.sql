e95361b51181e0fe9715e4d46ca510bc9d7b056f
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-11-20 14:37:58 -0600

    create role table and schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,12 +8,29 @@ VALUES
        (006, "Human Resources");
      
        
-INSERT INTO role (department_id,title, salary)
+INSERT INTO role (id,title, salary, department_id)
 VALUES 
-       (001, "Audio Engineer", 60000),
-       (002, "Accounts Administrator",52275),
-       (003, "Paralegal",  78478),
-       (004, "Digital Assistant", 62289),
-       (005, "Court Jester", 90000),
-       (006, "Benefits Manager", 130000);
+       (001, "Audio Engineer", 60000, 001),
+       (002, "Accounts Administrator",52275, 002),
+       (003, "Paralegal",  78478 , 003),
+       (004, "Digital Assistant", 62289 , 004),
+       (005, "Court Jester", 90000, 005),
+       (006, "Benefits Manager", 130000, 006);
      
+     INSERT INTO role (id,title, salary, department_id)
+VALUES 
+       (001, "Audio Engineer", 60000, 001),
+       (002, "Accounts Administrator",52275, 002),
+       (003, "Paralegal",  78478, 003),
+       (004, "Digital Assistant", 62289, 004),
+       (005, "Court Jester", 90000, 005),
+       (006, "Benefits Manager", 130000, 006);
+
+       INSERT INTO employee (id,first_name, last_name, role_id, manager_id )
+VALUES 
+       (001, "Cinar", "Jasper", 042, 126),
+       (002, "Angelika", "Church",012, 208),
+       (003, "Franklin", "Xi", 030, 166),
+       (004, "Elen","Villalobos", 066 , 012),
+       (005, "Nick", "Benson",007, 001),
+       (006, "Tobey", "Howell", 003, 015);
